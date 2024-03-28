54972acb3c3537bb52d46ecbeb345877bf9ea88f
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2024-03-28 07:36:51 +0100

    Finish the get and get by id patient

---
+++ src/app/services/patient.service.ts
@@ -0,0 +1,64 @@
+import { Injectable, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment.development';
+import { Patient } from '../patient';
+import { Observable } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
+@Injectable({
+  providedIn: 'root',
+})
+export class PatientService {
+  private url: string = environment.apiBaseUrl + '/Patient';
+  public patient!: Patient;
+  constructor(public http: HttpClient) {
+    this.fetchData();
+  }
+  async fetchData(): Promise<Patient[]> {
+    const data = await fetch(this.url);
+    return (await data.json()) ?? {};
+  }
+  async getPatientById(id: number): Promise<Patient | undefined> {
+    console.log(id);
+    const data = await fetch(`${this.url}/${id}`);
+    return (await data.json()) ?? {};
+  }
+  addPatient(
+    id: number,
+    code: string,
+    nom: string,
+    prenom: string,
+    sexe: string,
+    adresse: string,
+    profession: string,
+    gs: string,
+    rh: number,
+    race: string,
+    poids: number,
+    taille: number,
+    statutMatrimonial: string,
+    date_Naissance: string
+  ): Observable<Patient> {
+    const patient: Patient = {
+      id,
+      code,
+      nom,
+      prenom,
+      sexe,
+      adresse,
+      profession,
+      race,
+      poids,
+      taille,
+      rh,
+      gs,
+      statutMatrimonial,
+      date_Naissance,
+    };
+    return this.http.post<Patient>(this.url, patient, httpOptions);
+  }
+}
